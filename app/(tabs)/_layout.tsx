4ac5afed16eff2bc20cc172c3e914b100b1bfe73
CyberDuck <vitorcordero606@gmail.com>
2025-06-11 22:06:05 -0400

    Update better UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -20,15 +21,16 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
         tabBarStyle: {
           backgroundColor: TAB_BAR_BG,
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-          height: 80,
+          borderTopLeftRadius: 0,
+          borderTopRightRadius: 0,
+          height: 120,
           borderTopWidth: 0,
           position: 'absolute',
           left: 0,
           right: 0,
           bottom: 0,
           elevation: 10,
+          paddingHorizontal: 20,
         },
         tabBarLabelStyle: {
           display: 'none',
@@ -38,30 +40,84 @@ export default function TabLayout() {
         name="library"
         options={{
           title: 'Library',
-          tabBarIcon: ({ color }) => <IconSymbol size={40} name="library.books" color={TAB_BAR_ICON} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={styles.iconContainer}>
+              {focused && <View style={styles.activeBall} />}
+              <IconSymbol size={40} name="library.books" color={TAB_BAR_ICON} />
+              <Text style={styles.iconLabel}>Library</Text>
+            </View>
+          ),
         }}
       />
       <Tabs.Screen
         name="upload"
         options={{
           title: 'Upload',
-          tabBarIcon: ({ color }) => <IconSymbol size={40} name="add.box" color={TAB_BAR_ICON} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={styles.iconContainer}>
+              {focused && <View style={styles.activeBall} />}
+              <IconSymbol size={40} name="add.box" color={TAB_BAR_ICON} />
+              <Text style={styles.iconLabel}>+Upload</Text>
+            </View>
+          ),
         }}
       />
       <Tabs.Screen
         name="stats"
         options={{
           title: 'Stats',
-          tabBarIcon: ({ color }) => <IconSymbol size={40} name="bar.chart" color={TAB_BAR_ICON} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={styles.iconContainer}>
+              {focused && <View style={styles.activeBall} />}
+              <IconSymbol size={40} name="bar.chart" color={TAB_BAR_ICON} />
+              <Text style={styles.iconLabel}>Stats</Text>
+            </View>
+          ),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <IconSymbol size={40} name="person" color={TAB_BAR_ICON} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={styles.iconContainer}>
+              {focused && <View style={styles.activeBall} />}
+              <IconSymbol size={40} name="person" color={TAB_BAR_ICON} />
+              <Text style={styles.iconLabel}>Settings</Text>
+            </View>
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    width: 100,
+    alignSelf: 'center',
+    marginTop: 40,
+    position: 'relative',
+  },
+  iconLabel: {
+    fontSize: 14,
+    color: '#03C988',
+    marginTop: 1,
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  activeBall: {
+    position: 'absolute',
+    top: 10, //10
+    left: 10, //10
+    width: 80, //80
+    height: 80, //80
+    borderRadius: 64,
+    backgroundColor: '#1C82AD',
+    opacity: 0.27,
+    zIndex: 0,
+  },
+});
